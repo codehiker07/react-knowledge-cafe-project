c8d479e12d6f22fc0b113ad7771e9cacab57412f
codehiker07 <codehiker07@gmail.com>
2023-09-11 21:24:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl bg-gray-200'>React Knowledge Cafe Project</h1>
+    </>
+  )
+}
+
+export default App
