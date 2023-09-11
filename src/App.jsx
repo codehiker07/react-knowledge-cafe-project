03c91fd57239d314a3bdf117fe6618ef230f99d6
codehiker07 <codehiker07@gmail.com>
2023-09-11 22:15:35 +0600

    data fetch done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './Components/Header/Header'
+import Blogs from './Components/Blogs/Blogs'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-3xl bg-gray-200'>React Knowledge Cafe Project</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
