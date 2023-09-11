1cb8f841a9ef7c6011488a1121b13d1d28b03ed5
codehiker07 <codehiker07@gmail.com>
2023-09-12 00:03:45 +0600

    cover img call done

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
@@ -11,8 +12,13 @@ const Blogs = () => {
         },[])
 
     return (
-        <div>
-            
+        <div className="md:w-2/3">
+            <h2 className="text-2xl font-semibold">Blogs: { blogs.length }</h2>
+            {
+            blogs.map(blog=><Blog 
+                key={blog.id} 
+                blog={blog}></Blog>)
+            }
         </div>
     );
 };
