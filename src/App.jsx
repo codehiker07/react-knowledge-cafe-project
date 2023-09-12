8c99f3d68474d8f753c9eb0688650f2b8c75be40
codehiker07 <codehiker07@gmail.com>
2023-09-12 06:37:46 +0600

    bookmark btn added

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,17 @@ import Blogs from './Components/Blogs/Blogs'
 import Bookmarks from './Components/Bookmarks/Bookmarks'
 
 function App() {
+  const[bookmarks, setBookmarks]= useState([]);
+
+  const handleAddToBookmark = blog =>{
+    console.log('bookmark coming soon');
+  }
 
   return (
     <>
       <Header></Header>
-      <div className='md:flex '>
-      <Blogs></Blogs>
+      <div className='md:flex max-w-7xl mx-auto'>
+      <Blogs handleAddToBookmark = {handleAddToBookmark}></Blogs>
       <Bookmarks></Bookmarks>
       </div>
     </>
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Blog from "../Blog/Blog";
 
-const Blogs = () => {
+const Blogs = ({handleAddToBookmark}) => {
     const [blogs, setBlogs] = useState([]);
 
     useEffect(
@@ -15,9 +15,12 @@ const Blogs = () => {
         <div className="md:w-2/3">
             <h2 className="text-2xl font-semibold">Blogs: { blogs.length }</h2>
             {
-            blogs.map(blog=><Blog 
+                blogs.map(blog=><Blog 
                 key={blog.id} 
-                blog={blog}></Blog>)
+                blog={blog}
+                handleAddToBookmark={handleAddToBookmark}
+                >
+                </Blog>)
             }
         </div>
     );
